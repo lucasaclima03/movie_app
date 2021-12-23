c0fed43cf21a25b81598a98e3b0f63ddd6dc7ce8
Lucas Almeida <lucasaclima03@gmail.com>
2021-12-23 17:22:38 -0300

    finalização do modal

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -6,63 +6,24 @@ import 'package:movie_info_app/models/movie.dart';
 import 'categories.dart';
 import 'genres.dart';
 import 'movie_card.dart';
+import 'movie_carousel.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: const <Widget>[CategoryList(), Genres(), MovieCarousel()],
-    );
-  }
-}
-
-class MovieCarousel extends StatefulWidget {
-  const MovieCarousel({Key? key}) : super(key: key);
-
-  @override
-  _MovieCarouselState createState() => _MovieCarouselState();
-}
-
-class _MovieCarouselState extends State<MovieCarousel> {
-  late PageController _pageController;
-  int initialPage = 1;
-
-  @override
-  void initState() {
-    super.initState();
-    _pageController = PageController(
-      viewportFraction: 0.8,
-      initialPage: initialPage,
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _pageController.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(vertical: kDefaultPadding),
-      child: AspectRatio(
-        aspectRatio: 0.85,
-        child: PageView.builder(
-          controller: _pageController,
-          itemCount: movies.length,
-          itemBuilder: (context, index) => buildMovieSlider(index)
-        ),
+    return SingleChildScrollView(
+      child: Column(
+        children: const <Widget>[
+          CategoryList(),
+          Genres(),
+          SizedBox(
+            height: kDefaultPadding,
+          ),
+          MovieCarousel(),
+        ],
       ),
     );
   }
-
-  Widget buildMovieSlider(int index) {
-    return MovieCard(
-          movie: movies[index],
-        );
-  }
 }
-
---
+++ lib/screens/home/components/movie_carousel.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:movie_info_app/models/movie.dart';
+
+import '../../../constants.dart';
+import 'movie_card.dart';
+
+class MovieCarousel extends StatefulWidget {
+  const MovieCarousel({Key? key}) : super(key: key);
+
+  @override
+  _MovieCarouselState createState() => _MovieCarouselState();
+}
+
+class _MovieCarouselState extends State<MovieCarousel> {
+  late PageController _pageController;
+  int initialPage = 1;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController(
+      viewportFraction: 0.8,
+      initialPage: initialPage,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: kDefaultPadding),
+      child: AspectRatio(
+        aspectRatio: 0.85,
+        child: PageView.builder(
+            onPageChanged: (value) {
+              setState(() {
+                initialPage = value;
+              });
+            },
+            controller: _pageController,
+            physics: ClampingScrollPhysics(),
+            itemCount: movies.length,
+            itemBuilder: (context, index) => buildMovieSlider(index)),
+      ),
+    );
+  }
+
+  Widget buildMovieSlider(int index) => AnimatedBuilder(
+        animation: _pageController,
+        builder: (context, child) {
+          return AnimatedOpacity (
+            duration: Duration(milliseconds: 350),
+            opacity: initialPage == index ? 1 : 0.4,
+            child: MovieCard(movie: movies[index]));
+        },
+      );
+}
