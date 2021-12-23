b7580325663c14d8ce9e0f0946b85f4202072112
Lucas Almeida <lucasaclima03@gmail.com>
2021-12-22 21:43:54 -0300

    adicionando movie_card component

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:movie_info_app/components/genre_card.dart';
 import 'package:movie_info_app/constants.dart';
 import 'package:movie_info_app/models/movie.dart';
 import 'categories.dart';
 import 'genres.dart';
+import 'movie_card.dart';
 
 class Body extends StatelessWidget {
   const Body({Key? key}) : super(key: key);
@@ -11,7 +13,7 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: <Widget>[CategoryList(), Genres(), MovieCarousel()],
+      children: const <Widget>[CategoryList(), Genres(), MovieCarousel()],
     );
   }
 }
@@ -30,7 +32,10 @@ class _MovieCarouselState extends State<MovieCarousel> {
   @override
   void initState() {
     super.initState();
-    _pageController = PageController();
+    _pageController = PageController(
+      viewportFraction: 0.8,
+      initialPage: initialPage,
+    );
   }
 
   @override
@@ -46,51 +51,18 @@ class _MovieCarouselState extends State<MovieCarousel> {
       child: AspectRatio(
         aspectRatio: 0.85,
         child: PageView.builder(
+          controller: _pageController,
           itemCount: movies.length,
-          itemBuilder: (context, index) => MovieCard(
-            movie: movies[index],
-          )
+          itemBuilder: (context, index) => buildMovieSlider(index)
         ),
       ),
     );
   }
-}
 
-class MovieCard extends StatelessWidget {
-  final Movie movie;
-  const MovieCard({ Key? key, required this.movie }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                boxShadow: [kDefaultShadow],
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage(movie.poster)
-                )
-              ),
-
-            ) 
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(vertical: kDefaultPadding /2 ),
-            child: Text(
-              movie.title,
-              style: Theme.of(context).textTheme.headline5?.copyWith(
-                fontWeight: FontWeight.w600
-              ),
-            ),
-          )
-        ],
-        
-      ),
-    );
+  Widget buildMovieSlider(int index) {
+    return MovieCard(
+          movie: movies[index],
+        );
   }
 }
+
